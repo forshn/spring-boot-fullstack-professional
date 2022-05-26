8f0e1a2daaa3ce668cbf76174e4780938682c92b
Nikolay <forsh_nikolay@mail.ru>
2022-05-26 12:52:24 +0300

    first test add

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,29 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-	@Test
-	void contextLoads() {
-	}
+
+class DemoApplicationTests {
+	Calculator underTest = new Calculator();
+    @Test
+    void itShouldAddNumbers() {
+		// given
+		int numberOne = 20;
+		int numberTwo = 30;
+		// when
+		int result = underTest.add(numberOne, numberTwo);
+
+		//then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected).isLessThan(51);
+
+    }
+
+    class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
 
 }
